cf56235af111f2898e878a038ed46f1e04e3869c
Aleexsanndro <89137298+Aleexsanndro@users.noreply.github.com>
2021-09-09 11:46:27 -0300

    Finalizado!

---
+++ Model/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoTrabalhoEntra21.Model
+{
+    class Usuario
+    {
+        public int pontos { get; set; }
+        public string palavras { get; set; }
+    }
+}
